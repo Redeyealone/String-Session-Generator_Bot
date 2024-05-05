55af2ae91c67a2aea6ab12b6653a5f38cce0359c
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-05-06 04:45:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 class Config:
-    API_ID = int(os.environ.get('API_ID', 0))
-    API_HASH = str(os.environ.get('API_HASH', None))
-    BOT_TOKEN = str(os.environ.get('BOT_TOKEN', None))
-    MONGO_URI = str(os.environ.get('MONGO_URI', None))
-    UPDATES_CHANNEL = str(os.environ.get('UPDATES_CHANNEL', None)) #Start Without @
+    API_ID = int(os.environ.get('API_ID', 6435225))
+    API_HASH = str(os.environ.get('API_HASH', 4e984ea35f854762dcde906dce426c2d))
+    BOT_TOKEN = str(os.environ.get('BOT_TOKEN', 6571094339:AAGcENp3AmPPJslc88ERGsT_VdJ20hKTtvQ))
+    MONGO_URI = str(os.environ.get('MONGO_URI', mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority))
+    UPDATES_CHANNEL = str(os.environ.get('UPDATES_CHANNEL', Matching_pfp_Gallery)) #Start Without @
